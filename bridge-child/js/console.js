b549ebc8714d2fc315002e0711c15dcd2725f26d
JoseManuelCruzSanchez <44446487+JoseManuelCruzSanchez@users.noreply.github.com>
2019-03-21 18:39:54 +0100

    Add files via upload

--- bridge-child/js/console.js
+++ bridge-child/js/console.js
@@ -1,1 +1,12 @@
-console.log('aqui estamos');
+// Si tu script está en el header
+var respuesta;
+jQuery(document).ready(function($){
+    // Aquí tu script
+
+    $('.post_text_inner').each(function () {
+        respuesta = $(this);
+        console.log($(this));
+        $(this).context.style.backgroundColor = 'green';
+    });
+});
+
