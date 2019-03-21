bcf85415c6028599c74be8a8dbe8492261d78398
JoseManuelCruzSanchez <44446487+JoseManuelCruzSanchez@users.noreply.github.com>
2019-03-21 18:43:31 +0100

    Add files via upload

--- bridge-child/js/console.js
+++ bridge-child/js/console.js
@@ -1,12 +1,14 @@
+//http://www.juanvt.com/blog/usar-jquery-en-wordpress/
 // Si tu script está en el header
-var respuesta;
 jQuery(document).ready(function($){
     // Aquí tu script
-
     $('.post_text_inner').each(function () {
-        respuesta = $(this);
-        console.log($(this));
         $(this).context.style.backgroundColor = 'green';
     });
 });
+
+// Si tu script está en el footer
+/*(function ($){
+    // Aquí tu script
+})(jQuery);*/
 
