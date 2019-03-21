1a7fcfa7d9e944af552f7282a9745deaeca55144
JoseManuelCruzSanchez <44446487+JoseManuelCruzSanchez@users.noreply.github.com>
2019-03-21 19:48:16 +0100

    Add files via upload

---
+++ bridge-child/header-propio.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SantiagoPonce
+ * Date: 21/03/2019
+ * Time: 19:42
+ */
+?>
+
+<head>
+	<title>HOla que tal</title>
+</head>
---
+++ bridge-child/prueba_pagina_nueva.php
@@ -0,0 +1,26 @@
+<?php
+/*Template Name: nueva_pagina*/
+get_header('propio');
+?>
+
+<div class="title title_size_large  position_center " style="height:202px;">
+	<div class="image responsive"><img itemprop="image" src="http://localhost/wordpress-orsan/wordpress/wp-content/uploads/2016/02/title_image.jpg" alt="&nbsp;"> </div>
+	<div class="title_holder">
+		<div class="container">
+			<div class="container_inner clearfix">
+				<div class="title_subtitle_holder" style="padding-top:59.5px;">
+					<div class="title_subtitle_holder_inner">
+						<h1><span>Contact us</span></h1>
+						<span class="separator small center"></span>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+echo get_permalink();
+
+get_footer();
